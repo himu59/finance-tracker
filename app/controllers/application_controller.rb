4a76ae8ce36d7e3d5fdb22cb5bbfa8972253200e
Himanshu Negi <hnegi@MacBook-Pro.local>
2022-08-14 10:23:31 +0530

    Setup Login Authentication using Device gem and crete User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user! #Devise will create some helpers to use inside your controllers and views. To set up a controller with user authentication
 end
